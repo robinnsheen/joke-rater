0f7f6d1c2b6a3fef9cec2a992edb091d3be1c112
robin <robinnsheen@berkeley.edu>
2022-06-02 02:19:55 -0700

    added bootstrap styles

--- app.js
+++ app.js
@@ -2,14 +2,8 @@
 
 const scores = {};
 
-async function makeAndAddJokeDiv() {
-    const jokeObj = await axios.get('https://icanhazdadjoke.com/', {headers: {Accept:'application/json'}});
-
-    createHtml(jokeObj.data.joke);
-}
-
 function handleClickLike (evt) {
-    if ($(evt.target).attr("class") === "like btn") {
+    if ($(evt.target).hasClass("like")) {
         let scoreDiv = $(evt.target).siblings(".score");
         let score = Number(scoreDiv.text())+1;
         scoreDiv.text(score);
@@ -18,23 +12,23 @@ function handleClickLike (evt) {
 }
 
 function handleClickDislike (evt) {
-    if ($(evt.target).attr("class") === "dislike btn") {
+    if ($(evt.target).hasClass("dislike")) {
         let scoreDiv = $(evt.target).siblings(".score");
-        let score = Number(scoreDiv.text())-1;
-        scoreDiv.text(score);
+        let score = Number(scoreDiv.text());
+        if (score > 0) scoreDiv.text(score-1);
     }
 
 }
 
 function createLike() {
-    let likeBtn = $("<button></button").attr("class", "like btn")
+    let likeBtn = $("<button></button").attr("class", "like btn btn-primary mx-1")
                                        .on("click", handleClickLike)
                                        .text("Like");
     return likeBtn;
 }
 
 function createDislike() {
-    let dislikeBtn = $("<button></button").attr("class", "dislike btn")
+    let dislikeBtn = $("<button></button").attr("class", "dislike btn btn-danger mx-1")
                                           .on("click", handleClickDislike)
                                           .text("Dislike");
     return dislikeBtn;
@@ -49,15 +43,26 @@ function createScore() {
 
 function createHtml(joke) {
 
+    let jokeText = $(`<div>${joke}</div>`).attr("class", "joketext mb-3");
 
-    const html = $("<div></div").text(joke)
+
+    const html = $("<div></div")
                               .prepend(createScore())
+                              .prepend(createDislike())
                               .prepend(createLike())
-                              .prepend(createDislike());
+                              .prepend(jokeText)
+                              .attr("class", "jokediv border rounded border-3 p-4");
 
 
     $('.jokes').append(html);
 }
+
+async function makeAndAddJokeDiv() {
+    const jokeObj = await axios.get('https://icanhazdadjoke.com/', {headers: {Accept:'application/json'}});
+
+    createHtml(jokeObj.data.joke);
+}
+
 
 for(let i = 0; i < 10; i++) {
     makeAndAddJokeDiv();
