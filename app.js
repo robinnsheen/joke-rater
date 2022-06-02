a791f108cdc11c6df9d8e958a508af4ca8dd46ad
robin <robinnsheen@berkeley.edu>
2022-06-01 23:48:59 -0700

    added like, dislike, score to joke

--- app.js
+++ app.js
@@ -1,20 +1,55 @@
-"use strict"
+"use strict";
 
-async function getAndAddJoke() {
-    const joke = await axios.get('https://icanhazdadjoke.com/', {headers: {Accept:'application/json'}});
-    addJoke(joke.data);
-    return joke.data;
+async function makeAndAddJokeDiv() {
+    const jokeObj = await axios.get('https://icanhazdadjoke.com/', {headers: {Accept:'application/json'}});
+
+    createHtml(jokeObj.data.joke);
 }
 
-function addJoke(joke) { 
-    let html = createHtml(joke);
-    $('#jokes').append(html);
+function handleClickLike (evt) {
+    console.log(evt.target);
+}
+
+function handleClickDislike (evt) {
+    console.log(evt.target);
+}
+
+function createLike() {
+    let likeBtn = $("<button></button").attr("class", "like")
+                                       .attr("class", "btn")
+                                       .on("click", handleClickLike)
+                                       .text("Like");
+    return likeBtn;
+}
+
+function createDislike() {
+    let dislikeBtn = $("<button></button").attr("class", "dislike")
+                                          .attr("class", "btn")
+                                          .on("click", handleClickDislike)
+                                          .text("Dislike");
+    return dislikeBtn;
+}
+
+function createScore(id) {
+    let scoreDiv = $("<div></div").text(`0 likes`)
+                                  .attr("class", "score")
+                                  .attr("id", id);
+
+    return scoreDiv;
 }
 
 function createHtml(joke) {
-    let html = `<div>${joke.joke}</div><br>`
-    return html;
+
+
+    const html = $("<div></div").text(joke)
+                              .prepend(createScore(0))
+                              .prepend(createLike())
+                              .prepend(createDislike());
+
+
+    $('.jokes').append(html);
 }
+
 for(let i = 0; i < 10; i++) {
-    getAndAddJoke();
+    makeAndAddJokeDiv();
 }
