43721245f3a48ee71d810bebbd05b3d1bf9ab4c3
robin <robinnsheen@berkeley.edu>
2022-06-02 01:34:40 -0700

    added ability to upvote downvote

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const scores = {};
+
 async function makeAndAddJokeDiv() {
     const jokeObj = await axios.get('https://icanhazdadjoke.com/', {headers: {Accept:'application/json'}});
 
@@ -7,33 +9,40 @@ async function makeAndAddJokeDiv() {
 }
 
 function handleClickLike (evt) {
-    console.log(evt.target);
+    if ($(evt.target).attr("class") === "like btn") {
+        let scoreDiv = $(evt.target).siblings(".score");
+        let score = Number(scoreDiv.text())+1;
+        scoreDiv.text(score);
+    }
+
 }
 
 function handleClickDislike (evt) {
-    console.log(evt.target);
+    if ($(evt.target).attr("class") === "dislike btn") {
+        let scoreDiv = $(evt.target).siblings(".score");
+        let score = Number(scoreDiv.text())-1;
+        scoreDiv.text(score);
+    }
+
 }
 
 function createLike() {
-    let likeBtn = $("<button></button").attr("class", "like")
-                                       .attr("class", "btn")
+    let likeBtn = $("<button></button").attr("class", "like btn")
                                        .on("click", handleClickLike)
                                        .text("Like");
     return likeBtn;
 }
 
 function createDislike() {
-    let dislikeBtn = $("<button></button").attr("class", "dislike")
-                                          .attr("class", "btn")
+    let dislikeBtn = $("<button></button").attr("class", "dislike btn")
                                           .on("click", handleClickDislike)
                                           .text("Dislike");
     return dislikeBtn;
 }
 
-function createScore(id) {
-    let scoreDiv = $("<div></div").text(`0 likes`)
-                                  .attr("class", "score")
-                                  .attr("id", id);
+function createScore() {
+    let scoreDiv = $("<div></div").text(0)
+                                  .attr("class", "score");
 
     return scoreDiv;
 }
@@ -42,7 +51,7 @@ function createHtml(joke) {
 
 
     const html = $("<div></div").text(joke)
-                              .prepend(createScore(0))
+                              .prepend(createScore())
                               .prepend(createLike())
                               .prepend(createDislike());
 
